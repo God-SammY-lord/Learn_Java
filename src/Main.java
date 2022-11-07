092bdec84b06ffa5dda315731d03c9a8a8546307
God_Sammy <sahilblaster456@gmail.com>
2022-11-07 23:04:09 +0530

    7th November 22

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+//    Public implies Anyone can access this class
+//    Static denotes that this method is the own method of the Class "Main"
+//    Void Implies that the program doesn't return anything
+    public static void main(String[] args) {
+//        Simple Print statement
+        System.out.println("Hello World");
+    }
+}
