f40cba948f087c0653407d2976a690141c726acc
God_Sammy <sahilblaster456@gmail.com>
2022-11-09 00:53:35 +0530

    7th November 22

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
 //    Public implies Anyone can access this class
 //    Static denotes that this method is the own method of the Class "Main"
 //    Void Implies that the program doesn't return anything
     public static void main(String[] args) {
 //        Simple Print statement
-        System.out.println("Hello World, Bye");
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        double d = scan.nextDouble();
+        String s = scan.nextLine();
+        scan.close();
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
     }
 }
